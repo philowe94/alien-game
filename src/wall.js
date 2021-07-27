e12613d658f1a44962c3ad526d74c4c1c75fe989
Philip Lowe <philip.lowe94@gmail.com>
2021-07-26 20:26:07 -0400

    framework for multiple levels complete

--- src/wall.js
+++ src/wall.js
@@ -4,6 +4,8 @@ class Wall extends Tile {
     constructor(options) {
         super(options);
         this.color = "#114b5f"
+        this.sprites = new Image();
+        this.sprites.src = 'assets/map-tiles.png';
     }
 
     render(ctx) {
@@ -12,6 +14,16 @@ class Wall extends Tile {
             (this.pos[0] * 64), 
             (this.pos[1] * 64), 
             64, 
+            64
+        );
+
+        ctx.drawImage(
+            this.sprites,
+            0,0,
+            16,16,
+            (this.pos[0] * 64),
+            (this.pos[1] * 64),
+            64,
             64
         );
     }
